8cd008ef2d7bd957f5e18cc9bc27eab638fb85b0
Joao Oliveira <jhcardosodeoliveira@gmail.com>
2026-04-27 12:48:11 +0100

    auth.py created in dependencies, this makes my current user and admim dependent from login

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,13 @@ from app.config import get_settings
 from app.utils.redis_client import init_redis, close_redis
 from app.routers.auth import router as auth_router
 
+# ------------ Imports for Test Route ------------
+from fastapi import Depends
+from app.models.user import User
+from app.dependencies.auth import get_current_user
+from app.schemas.user import UserResponse
+
+
 settings = get_settings()
 logger = structlog.get_logger()
 
@@ -58,6 +65,14 @@ app.add_middleware(
 # ------------ Routers ------------
 
 app.include_router(auth_router)
+
+
+# ------------ Protected Test Route (It will be taken out in phase 3) ------------
+
+@app.get("/me", response_model=UserResponse, tags=["System"])
+async def get_me(current_user: User = Depends(get_current_user)):
+    """Returns the currently authenticated user. Protected route."""
+    return current_user
 
 
 # ------------ Health ------------
